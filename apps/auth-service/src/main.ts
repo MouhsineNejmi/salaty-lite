2f2606730de3a7d2bf7d9fe5ddf457ae53bd7633
MouhsineNejmi <nejmimouhsine@gmail.com>
2025-09-29 14:50:07 +0100

    implement rate limiter, api gateway proxy also error handler middle

--- apps/auth-service/src/main.ts
+++ apps/auth-service/src/main.ts
@@ -1,10 +1,14 @@
 import express, { type Request, type Response } from 'express';
+import cookieParser from 'cookie-parser';
+import { errorMiddleware } from '@salaty/common';
 
 const app = express();
 
 app.get('/', (req: Request, res: Response) => {
   res.send({ message: 'Welcome to Auth Service!' });
 });
+
+app.use(errorMiddleware);
 
 const port = process.env.PORT || 4001;
 const server = app.listen(port, () => {
---
+++ packages/common/index.d.ts
@@ -0,0 +1,3 @@
+export * from './error-handler';
+export * from './error-handler/error-middleware';
+//# sourceMappingURL=index.d.ts.map
